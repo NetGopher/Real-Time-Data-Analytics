8841bd603de739ad038deb82c1dcad736febc53f
Baymane <ayman.boubleh@gmail.com>
2021-01-05 17:42:53 +0100

    added npm dependencies

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
