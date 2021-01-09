3dff15d3119c911043cfc931871ed70641bc3104
Baymane <ayman.boubleh@gmail.com>
2021-01-09 13:22:15 +0100

    deleted 'kafka-streams-'

--- kafka-streams-/src/main/java/enset/bdcc/stage/kafkastreams2/config/StreamType.java
+++
@@ -1,9 +0,0 @@
-package enset.bdcc.stage.kafkastreams2.config;
-
-
-public class StreamType {
-    public static final String ERROR = "ERROR";
-    public static final String COUNT = "COUNT_STREAM";
-    public static final String REDDIT_MENTIONS = "REDDIT_MENTIONS";
-
-}
